60b9b913ce59b9cda4674de60cc4c567ff3064b6
artem <art.kisten@gmail.com>
2018-10-20 17:55:36 +0300

    консольное приложение

---
+++ Консольное приложение/ConsoleApplicationHomework/ConsoleApplicationHomework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplicationHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
